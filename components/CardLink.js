1376fa9f1fd0c37ef442e92fec8d3d3d6ed67ca6
christopherleja <christopher.leja@gmail.com>
2021-11-01 21:50:24 -0400

    add project modal, refactor

--- components/CardLink.js
+++ components/CardLink.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-const CardLink = ({ title, link, fullWidth }) => {
-  const width = fullWidth ? "w-3/5 sm:w-1/2 mx-auto" : "w-2/5 sm:w-1/5";
+const CardLink = ({ title, link }) => {
   return (
     <>
       {link ? (
-        <div className={width + " mt-4 rounded-full border-2 bg-white"}>
+        <div className="w-3/5 sm:w-1/2 mx-auto mt-4 rounded-full border-2 bg-blueDark hover:opacity-80 mb-8">
           <a href={link} target="_blank" rel="noreferrer">
-            <p className="text-center text-blue-600">{title}</p>
+            <p className="text-center text-grayLight">{title}</p>
           </a>
         </div>
       ) : null}
--- components/DetailModal.js
+++ components/DetailModal.js
@@ -1,36 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import AppContext from "../context";
 import Image from "next/image";
+import RichText from "./RichText";
+import CardLink from "./CardLink";
 
 const DetailModal = () => {
   const { selected, setModalVisible, modalVisible } = useContext(AppContext);
+  const [imgUrl, setImgUrl] = useState("");
 
-  let imgUrl = "";
-
-  if (selected?.image) {
-    imgUrl = "https:" + selected?.image?.fields?.file?.url;
-  }
-
+  useEffect(() => {
+    if (selected) {
+      setImgUrl(`https:${selected?.image?.fields?.file?.url}`);
+    }
+  }, [selected]);
   return (
     <>
-      {modalVisible && (
+      {modalVisible && imgUrl && (
         <div
           id="modal"
-          className="z-10 absolute w-full h-full"
+          className="z-10 absolute w-full h-full delay-600 px-4"
           onClick={() => setModalVisible(false)}
         >
           <div className="project-modal p-8">
-            <h1 className="h1 text-blueDark">{selected.title || "test"}</h1>
-            <div className="flex flex-col flex-wrap md:w-1/2">
-              <div className="">
+            <h1 className="h1 mb-8 text-blueDark">{selected.title}</h1>
+            {selected.url && (
+              <CardLink title="Visit" link={selected.url} fullWidth />
+            )}
+            <div className="flex flex-col lg:flex-row">
+              <div className="w-full lg:w-1/3 p-8">
                 <Image
                   src={imgUrl}
                   layout="responsive"
-                  height={selected.modalVisible ? 400 : 300}
-                  width={selected.modalVisible ? 300 : 400}
+                  height={selected.mobileView ? 400 : 300}
+                  width={selected.mobileView ? 300 : 400}
                   objectFit="contain"
                   alt={selected.title}
                 />
+              </div>
+              <div className="w-full lg:w-2/3">
+                <RichText text={selected.expandedDescription} />
               </div>
             </div>
           </div>
