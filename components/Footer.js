989bff2d1901ca064e0780c0ef8e5fc716ad609d
christopherleja <christopher.leja@gmail.com>
2021-11-02 01:17:35 -0400

    fix footer test color

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import Contact from "./Contact";
 const Footer = () => {
   return (
     <div className="my-8">
-      <h3 className="h5 text-sienna font-semibold mb-4">Christopher Leja</h3>
+      <h3 className="h5 text-blueDark font-semibold mb-4">Christopher Leja</h3>
       <h6 className="h6 text-center text-blueDark mb-2">Somerville, MA</h6>
       <p className="text-blueDark text-center mb-4">
         christopher.leja@gmail.com
