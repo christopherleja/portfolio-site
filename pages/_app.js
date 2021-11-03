551b7866b6f1c89ade9f6ff7796c96cd6b1b358d
christopherleja <christopher.leja@gmail.com>
2021-11-03 01:16:22 -0400

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import "../styles/main.scss";
 import "tailwindcss/tailwind.css";
+import { useEffect } from "react";
+import ReactGA from "react-ga";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    ReactGA.initialize(process.env.GOOGLE_ID);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
